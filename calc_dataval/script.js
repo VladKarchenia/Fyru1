195e9dd7ad5e8b09d640305c1cd979d899a17d22
VladKarchenia <karchenia@mail.ru>
2018-06-19 11:17:02 +0300

    replaced double quotes for single

--- calc_dataval/script.js
+++ calc_dataval/script.js
@@ -1,97 +1,108 @@
-"use scrict"
+'use scrict'
 
-var input = document.getElementById("input"),
-    equals = document.getElementById("result"),
-    clear = document.getElementById("memoryClear"),
-    memoryAdd = document.getElementById("memoryAdd"),
-    memorySub = document.getElementById("memorySub"),
-    memorySave = document.getElementById("memorySave"),
-    nums = document.querySelectorAll(".num"),
-    ops = document.querySelectorAll(".operation"),
-    theNum = "",
-    oldNum = "",
-    memory = "",
+var input = document.getElementById('input'),
+    equals = document.getElementById('result'),
+    clear = document.getElementById('memoryClear'),
+    memoryAdd = document.getElementById('memoryAdd'),
+    memorySub = document.getElementById('memorySub'),
+    memorySave = document.getElementById('memorySave'),
+    nums = document.querySelectorAll('.num'),
+    ops = document.querySelectorAll('.operation'),
+    theNum = '',
+    oldNum = '',
+    memory = '',
     resultNum,
+    hasPoint = false,
     operation;
 
 var setNum = function() {
+    const value = this.dataset.val
+    const isPoint = value === '.'
+
+    if (isPoint) {
+        if (hasPoint) {
+            return
+        }
+        hasPoint = true
+    }
+
     if (resultNum) {
-        theNum = this.getAttribute("data-val");
-        resultNum = "";
+        theNum = value;
+        resultNum = '';
     } else {
-        theNum += this.getAttribute("data-val");
+        theNum += value;
     }
     input.innerHTML = theNum;
 }
 
 var moveNum = function() {
+    hasPoint = false
     oldNum = theNum;
-    theNum = "";
-    operation = this.getAttribute("data-ops");
-    equals.setAttribute("data-result", "");
+    theNum = '';
+    operation = this.getAttribute('data-ops');
+    equals.setAttribute('data-result', '');
 }
 
 var displayNum = function() {
+    hasPoint = false
     oldNum = parseFloat(oldNum);
     theNum = parseFloat(theNum);
     switch (operation) {
-        case "plus":
+        case 'plus':
             resultNum = oldNum + theNum;
             break;
   
-        case "minus":
+        case 'minus':
             resultNum = oldNum - theNum;
             break;
   
-        case "times":
+        case 'times':
             resultNum = oldNum * theNum;
             break;
   
-        case "divided by":
+        case 'divided by':
             resultNum = oldNum / theNum;
             break;
 
         default:
         resultNum = theNum;
     }
+    resultNum = +resultNum.toFixed(9)
     input.innerHTML = resultNum;
-    equals.setAttribute("data-result", resultNum);
+    equals.setAttribute('data-result', resultNum);
     oldNum = 0;
     theNum = resultNum;
 }
 
 var memoryOp = function() {
     switch (operation) {
-        case "memorySave":
+        case 'memorySave':
             memory = theNum;
             break;
 
-        case "memorySub":
-            memory = memory.length > 0 ? (memory + "-" + theNum) : (theNum.length > 0 ? theNum : "");
+        case 'memorySub':
+            memory = memory.length > 0 ? (memory + '-' + theNum) : (theNum.length > 0 ? theNum : '');
             break;
 
-        case "memoryAdd":
-            memory = memory.length > 0 ? (memory + "+" + theNum) : (theNum.length > 0 ? theNum : "");
+        case 'memoryAdd':
+            memory = memory.length > 0 ? (memory + '+' + theNum) : (theNum.length > 0 ? theNum : '');
             break;
-
-        default:
-        console.log("error");
     }
     input.innerHTML = resultNum;
-    equals.setAttribute("data-result", resultNum);
+    equals.setAttribute('data-result', resultNum);
     oldNum = 0;
     theNum = resultNum;
 }
 
 var memoryClear = function() {
-    memory = "";
+    memory = '';
 }
 
 var clearAll = function() {
-    oldNum = "";
-    theNum = "";
-    input.innerHTML = "0";
-    equals.setAttribute("data-result", resultNum);
+    oldNum = '';
+    theNum = '';
+    input.innerHTML = '0';
+    equals.setAttribute('data-result', resultNum);
 }
 
 for (var i = 0; i < nums.length; i++) {
