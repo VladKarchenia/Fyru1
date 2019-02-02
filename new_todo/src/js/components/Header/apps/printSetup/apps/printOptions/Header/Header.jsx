085eb54cae7630699ba2f972e74508ff72fde115
Fyru <fyruvlad@gmail.com>
2019-02-02 19:04:35 +0300

    fix custom header/footer

--- new_todo/src/js/components/Header/apps/printSetup/apps/printOptions/Header/Header.jsx
+++ new_todo/src/js/components/Header/apps/printSetup/apps/printOptions/Header/Header.jsx
@@ -1,22 +1,17 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 
 import styles from './Header.module.scss'
 
-class Header extends PureComponent {
-  render () {
-    const { listName, printEnd } = this.props
-    return (
-      <div className={styles.header}>
-        <div className={styles.listname}>
-          <span className={styles.span_listname}>{listName}</span>
-        </div>
-        <div className={styles.header_buttons}>
-          <button className={styles.calcel_btn} onClick={printEnd}>cancel</button>
-          <button className={styles.print_btn} onClick={() => print()}>print</button>
-        </div>
-      </div>
-    )
-  }
-}
+const Header = ({ listName, printEnd }) => (
+  <div className={styles.header}>
+    <div className={styles.listname}>
+      <span className={styles.span_listname}>{listName}</span>
+    </div>
+    <div className={styles.header_buttons}>
+      <button className={styles.calcel_btn} onClick={printEnd}>cancel</button>
+      <button className={styles.print_btn} onClick={() => print()}>print</button>
+    </div>
+  </div>
+)
 
 export default Header
